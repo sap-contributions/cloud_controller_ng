d81e0433f1ae456a8900e9c594752699e5b8ea91
Jochen Ehret <jochen.ehret@sap.com>
2022-03-28 15:06:44 +0200

    add require 'rubygems'

--- config/boot.rb
+++ config/boot.rb
@@ -4,6 +4,7 @@ $LOAD_PATH.unshift(File.expand_path('../lib', __dir__))
 $LOAD_PATH.unshift(File.expand_path('../app', __dir__))
 $LOAD_PATH.unshift(File.expand_path('../middleware', __dir__))
 
+require 'rubygems'
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 
 OpenTelemetry::SDK.configure do |c|

cd4bc7d9d962175a0a82a56390c382b4d92f0e69
I546215 <a.alam@sap.com>
2022-03-31 16:12:00 +0200

    add pg and deplayed gems

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -1,10 +1,14 @@
 require 'opentelemetry/sdk'
+require 'opentelemetry-instrumentation-delayed_job'
 require 'opentelemetry-instrumentation-sinatra'
+require 'opentelemetry-instrumentation-pg'
 require 'opentelemetry/exporter/otlp'
 
 module CCInitializers
   def self.opentelemetry(cc_config)
     OpenTelemetry::SDK.configure do |c|
+      c.use 'OpenTelemetry::Instrumentation::DelayedJob'
+      c.use 'OpenTelemetry::Instrumentation::PG'
       c.use 'OpenTelemetry::Instrumentation::Sinatra'
     end
   end

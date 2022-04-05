7226cfbc0402bb29585558bc326950ee3ca47c2c
Aftab Alam <81828613+iaftab-alam@users.noreply.github.com>
2022-04-05 16:42:25 +0200

    Enable and test all instrumentation libraries

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -1,15 +1,17 @@
-require 'opentelemetry/sdk'
-require 'opentelemetry-instrumentation-delayed_job'
-require 'opentelemetry-instrumentation-sinatra'
-require 'opentelemetry-instrumentation-pg'
+require 'opentelemetry/instrumentation/all'
 require 'opentelemetry/exporter/otlp'
+require 'opentelemetry/sdk'
 
 module CCInitializers
   def self.opentelemetry(cc_config)
+    otel_exporter = OpenTelemetry::Exporter::OTLP::Exporter.new
+    processor = OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(otel_exporter)
+
     OpenTelemetry::SDK.configure do |c|
-      c.use 'OpenTelemetry::Instrumentation::DelayedJob'
-      c.use 'OpenTelemetry::Instrumentation::PG'
-      c.use 'OpenTelemetry::Instrumentation::Sinatra'
+      c.service_name = 'cf_cloud_controller'
+      c.service_version = '1.128.0dev.2'
+      c.add_span_processor(processor)
+      c.use_all()
     end
   end
 end

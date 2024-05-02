0227023daf9ce725c8992917ed5b6e51c2379c42
Pavel Busko <pavel.busko@sap.com>
2024-05-02 10:54:33 +0200

    Add cnb_lifecycle_data model
    Co-authored-by: Ralf Pannemans <ralf.pannemans@sap.com>
    Co-authored-by: Johannes Dillmann <j.dillmann@sap.com>
    Co-authored-by: Nicolas Bender <nicolas.bender@sap.com>

---
+++ db/migrations/20240313130000_create_cnb_lifecycle_data.rb
@@ -0,0 +1,28 @@
+Sequel.migration do
+  change do
+    create_table(:cnb_lifecycle_data) do
+      VCAP::Migration.common(self, :cnb_lifecycle_data)
+
+      String :build_guid, size: 255
+      foreign_key [:build_guid], :builds, key: :guid, name: :fk_cnb_lifecycle_build_guid, on_delete: :cascade
+      index [:build_guid], name: :fk_cnb_lifecycle_build_guid_index
+
+      String :app_guid, size: 255
+      index [:app_guid], name: :fk_cnb_lifecycle_app_guid_index
+
+      String :droplet_guid, size: 255
+      index [:droplet_guid], name: :fk_cnb_lifecycle_droplet_guid_index
+
+      String :stack, size: 255
+    end
+
+    alter_table(:buildpack_lifecycle_buildpacks) do
+      add_column :cnb_lifecycle_data_guid, String, null: true, size: 255
+
+      add_foreign_key [:cnb_lifecycle_data_guid], :cnb_lifecycle_data, key: :guid, name: :fk_blcnb_bldata_guid, on_delete: :cascade
+      add_index [:cnb_lifecycle_data_guid], name: :bl_cnb_bldata_guid_index
+
+      set_column_allow_null :cnb_lifecycle_data_guid
+    end
+  end
+end

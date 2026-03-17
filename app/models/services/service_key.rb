0b73ef979f77c1ea296a1edf0f076410f1620a1e
Serdar Özer <serdar.oezer@sap.com>
2026-03-17 13:01:56 +0100

    fix: service_broker validate_unique reintroduced because, it uses this validation in service_key_manager to refraing http call in service_key_create

--- app/models/services/service_key.rb
+++ app/models/services/service_key.rb
@@ -49,6 +49,7 @@ module VCAP::CloudController
     def validate
       validates_presence :name
       validates_presence :service_instance
+      validates_unique %i[name service_instance_id]
 
       return unless service_instance
 

ffe557e018115c3c3f35a981ac6b94496974580f
Serdar Özer <serdar.oezer@sap.com>
2026-03-24 15:45:23 +0100

    feat: concurrent migration for postgres db

--- db/migrations/20260323144429_add_unique_constraint_to_revision_process_commands.rb
+++ db/migrations/20260323144429_add_unique_constraint_to_revision_process_commands.rb
@@ -1,4 +1,6 @@
-Sequel.migration do
+Sequel.migration do # rubocop:disable Metrics/BlockLength
+  no_transaction # required for concurrently option on postgres
+
   up do
     transaction do
       duplicates = self[:revision_process_commands].
@@ -15,20 +17,44 @@ Sequel.migration do
                         map(:id)
         self[:revision_process_commands].where(id: ids_to_remove).delete
       end
+    end
 
+    if database_type == :postgres
+      VCAP::Migration.with_concurrent_timeout(self) do
+        add_index :revision_process_commands, %i[revision_guid process_type],
+                  name: :revision_process_commands_revision_guid_process_type_index,
+                  unique: true,
+                  concurrently: true,
+                  if_not_exists: true
+      end
+    else
       alter_table(:revision_process_commands) do
+        # rubocop:disable Sequel/ConcurrentIndex -- MySQL does not support concurrent index operations
         unless @db.indexes(:revision_process_commands).key?(:revision_process_commands_revision_guid_process_type_index)
-          add_unique_constraint %i[revision_guid process_type],
-                                name: :revision_process_commands_revision_guid_process_type_index
+          add_index %i[revision_guid process_type], unique: true,
+                                                    name: :revision_process_commands_revision_guid_process_type_index
         end
+        # rubocop:enable Sequel/ConcurrentIndex
       end
     end
   end
 
   down do
-    alter_table(:revision_process_commands) do
-      if @db.indexes(:revision_process_commands).key?(:revision_process_commands_revision_guid_process_type_index)
-        drop_constraint(:revision_process_commands_revision_guid_process_type_index, type: :unique)
+    if database_type == :postgres
+      VCAP::Migration.with_concurrent_timeout(self) do
+        drop_index :revision_process_commands, nil,
+                   name: :revision_process_commands_revision_guid_process_type_index,
+                   concurrently: true,
+                   if_exists: true
+      end
+    else
+      alter_table(:revision_process_commands) do
+        # rubocop:disable Sequel/ConcurrentIndex -- MySQL does not support concurrent index operations
+        if @db.indexes(:revision_process_commands).key?(:revision_process_commands_revision_guid_process_type_index)
+          drop_index %i[revision_guid process_type],
+                     name: :revision_process_commands_revision_guid_process_type_index
+        end
+        # rubocop:enable Sequel/ConcurrentIndex
       end
     end
   end

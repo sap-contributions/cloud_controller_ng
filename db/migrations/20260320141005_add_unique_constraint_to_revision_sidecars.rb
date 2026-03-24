012595333b366c84dafbe363cfa55edbc0466d02
Serdar Özer <serdar.oezer@sap.com>
2026-03-24 16:10:53 +0100

    feat: concurrent migration for postgres db

--- db/migrations/20260320141005_add_unique_constraint_to_revision_sidecars.rb
+++ db/migrations/20260320141005_add_unique_constraint_to_revision_sidecars.rb
@@ -1,7 +1,9 @@
 Sequel.migration do
+  no_transaction # required for concurrently option on postgres
+
   up do
-    # remove duplicate entries if they exist
     transaction do
+      # remove duplicate entries if they exist
       duplicates = self[:revision_sidecars].
                    select(:revision_guid, :name).
                    group(:revision_guid, :name).
@@ -16,19 +18,42 @@ Sequel.migration do
                         map(:id)
         self[:revision_sidecars].where(id: ids_to_remove).delete
       end
+    end
 
+    if database_type == :postgres
+      VCAP::Migration.with_concurrent_timeout(self) do
+        add_index :revision_sidecars, %i[revision_guid name],
+                  name: :revision_sidecars_revision_guid_name_index,
+                  unique: true,
+                  concurrently: true,
+                  if_not_exists: true
+      end
+    else
       alter_table(:revision_sidecars) do
+        # rubocop:disable Sequel/ConcurrentIndex -- MySQL does not support concurrent index operations
         unless @db.indexes(:revision_sidecars).key?(:revision_sidecars_revision_guid_name_index)
-          add_unique_constraint(%i[revision_guid name],
-                                name: :revision_sidecars_revision_guid_name_index)
+          add_index %i[revision_guid name], unique: true,
+                                            name: :revision_sidecars_revision_guid_name_index
         end
+        # rubocop:enable Sequel/ConcurrentIndex
       end
     end
   end
 
   down do
-    alter_table(:revision_sidecars) do
-      drop_constraint(:revision_sidecars_revision_guid_name_index, type: :unique) if @db.indexes(:revision_sidecars).key?(:revision_sidecars_revision_guid_name_index)
+    if database_type == :postgres
+      VCAP::Migration.with_concurrent_timeout(self) do
+        drop_index :revision_sidecars, nil,
+                   name: :revision_sidecars_revision_guid_name_index,
+                   concurrently: true,
+                   if_exists: true
+      end
+    else
+      alter_table(:revision_sidecars) do
+        # rubocop:disable Sequel/ConcurrentIndex -- MySQL does not support concurrent index operations
+        drop_index %i[revision_guid name], name: :revision_sidecars_revision_guid_name_index if @db.indexes(:revision_sidecars).key?(:revision_sidecars_revision_guid_name_index)
+        # rubocop:enable Sequel/ConcurrentIndex
+      end
     end
   end
 end

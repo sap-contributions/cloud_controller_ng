9794648243f8fecae0ff50e796deb9e3fa940a12
Serdar Özer <serdar.oezer@sap.com>
2026-04-07 18:00:41 +0200

    fix: comments are fixed

--- spec/migrations/20260320141005_add_unique_constraint_to_revision_sidecars_spec.rb
+++ spec/migrations/20260320141005_add_unique_constraint_to_revision_sidecars_spec.rb
@@ -9,38 +9,26 @@ RSpec.describe 'add unique constraint to revision sidecar types', isolation: :tr
   let!(:revision) { VCAP::CloudController::RevisionModel.make(:app) }
 
   it 'remove duplicates, add constraint and revert migration' do
-    # =========================================================================================
-    # SETUP: Create duplicate entries to test the de-duplication logic.
-    # =========================================================================================
+    # create duplicate entries
     db[:revision_sidecars].insert(guid: SecureRandom.uuid, name: 'app', command: 'command', revision_guid: revision.guid)
     db[:revision_sidecars].insert(guid: SecureRandom.uuid, name: 'app', command: 'command', revision_guid: revision.guid)
     expect(db[:revision_sidecars].where(name: 'app', revision_guid: revision.guid).count).to eq(2)
 
-    # =========================================================================================
-    # UP MIGRATION: Run the migration to apply the unique constraints.
-    # ========================================================================================
+    # run the migration
     Sequel::Migrator.run(db, migrations_path, target: current_migration_index, allow_missing_migration_files: true)
 
-    # =========================================================================================
-    # ASSERT UP MIGRATION: Verify that duplicates are removed and constraints are enforced.
-    # =========================================================================================
+    # verify duplicates are removed and constraint is enforced
     expect(db[:revision_sidecars].where(name: 'app', revision_guid: revision.guid).count).to eq(1)
     expect(db.indexes(:revision_sidecars)).to include(:revision_sidecars_revision_guid_name_index)
     expect { db[:revision_sidecars].insert(guid: SecureRandom.uuid, name: 'app', command: 'command', revision_guid: revision.guid) }.to raise_error(Sequel::UniqueConstraintViolation)
 
-    # =========================================================================================
-    # TEST IDEMPOTENCY: Running the migration again should not cause any errors.
-    # =========================================================================================
+    # running the migration again should not cause any errors
     expect { Sequel::Migrator.run(db, migrations_path, target: current_migration_index, allow_missing_migration_files: true) }.not_to raise_error
 
-    # =========================================================================================
-    # DOWN MIGRATION: Roll back the migration to remove the constraints.
-    # =========================================================================================
+    # roll back the migration
     Sequel::Migrator.run(db, migrations_path, target: current_migration_index - 1, allow_missing_migration_files: true)
 
-    # =========================================================================================
-    # ASSERT DOWN MIGRATION: Verify that constraints are removed and duplicates can be re-inserted.
-    # =========================================================================================
+    # verify constraint is removed and duplicates can be re-inserted
     expect(db.indexes(:revision_sidecars)).not_to include(:revision_sidecars_revision_guid_name_index)
     expect { db[:revision_sidecars].insert(guid: SecureRandom.uuid, name: 'app', command: 'command', revision_guid: revision.guid) }.not_to raise_error
   end
